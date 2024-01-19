dc71e951c9cdbc018c738ee08743a23dc1fb5324
sumayaa4406037 <149380178+sumayaa4406037@users.noreply.github.com>
2024-01-19 19:07:15 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,26 @@
+// const var let
+// var x=10;
+// var y=50;
+// document.write(x+y);
+// window.alert(x+y);
+// console.log(x+y);
+// var id,sname,classname;
+// id=4406037;
+// sname=' Sumaya ';
+// classname=' six ';
+// document.write(sname+id+classname);
+// let x=50;
+// let y=20;
+// document.write(x+y);
+
+// document.getElementById('demo').style.color='red';
+document.getElementById('demo').style.fontSize='30px';
+// document.getElementById('demo').style.border='2px solid red';
+
+
+// document.getElementsByClassName('pera')[0].style.color='green';
+
+// document.getElementsByTagName('p')[0].style.color='blue';
+document.querySelector('.pera'). style.color='black';
+document.querySelector('#demo'). style.border='2px solid black'; 
+document.querySelector('p'). style.background='gray'; 
